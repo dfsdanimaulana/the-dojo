c1487d304bd4af7bae2ff97dc29fa5570f715c44
dfsdanimaulana <danimaulana9f@gmail.com>
2021-12-15 10:55:18 +0700

    firebase init

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHgZUQvhpV-AuKDMSdPFYxV1ZHh21nFjU",
+  authDomain: "thedojosite-686bb.firebaseapp.com",
+  projectId: "thedojosite-686bb",
+  storageBucket: "thedojosite-686bb.appspot.com",
+  messagingSenderId: "515089564943",
+  appId: "1:515089564943:web:d3251777b84b6ca48daa7a"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+// init firestore
+const db = firebase.firestore()
+
+// init authentication
+const auth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { db, auth, timestamp }
